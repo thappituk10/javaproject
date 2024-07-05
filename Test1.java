f34c3a533203e4c7b47df7a6dbbbde551cbc2aa6
thappituk10 <6610122113045@pnru.ac.th>
2024-07-05 11:13:23 +0700

    first commit

---
+++ Test1.java
@@ -0,0 +1,11 @@
+/**
+ * Test1
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("A cat is on a mat.");
+        System.out.println("A cat is red");
+    }
+}
